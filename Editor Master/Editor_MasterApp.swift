080d417e01cbac8cae4e2a6373cb2afaa3adce28
tamal-web <tamalkchhabra2007@outlook.com>
2024-06-05 04:32:49 +0530

    Initial Commit

---
+++ Editor Master/Editor_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Editor_MasterApp.swift
+//  Editor Master
+//
+//  Created by Tamal on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Editor_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
